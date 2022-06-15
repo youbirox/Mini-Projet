09caa13fd720cd6cb6b189561e4ce696eae40e9a
Ayoub Moutii <youbirox1994@gmail.com>
2022-06-15 15:11:56 +0200

    Correct page profil

--- js/profil.js
+++ js/profil.js
@@ -41,31 +41,73 @@ function logout()
 
 function NomEdit()
 {
-    document.getElementById("Nom").innerHTML='<input type="text"/>&ensp; &ensp;<i class="fa-solid fa-check"></i>'
+    document.getElementById("Nom").innerHTML='<input type="text" id="NomE"/>&ensp; &ensp;<i onclick="NomSave()" class="fa-solid fa-check"></i>'
+}
+function NomSave()
+{
+    var NomE = document.getElementById("NomE").value
+    window.localStorage.setItem('UserNom', NomE);
+    window.location = window.location
 }
 function PrenomEdit()
 {
-    document.getElementById("Prenom").innerHTML='<input type="text"/>&ensp; &ensp;<i class="fa-solid fa-check"></i>'
+    document.getElementById("Prenom").innerHTML='<input type="text" id="PrenomE"/>&ensp; &ensp;<i onclick="PrenomSave()" class="fa-solid fa-check"></i>'
+}
+function PrenomSave()
+{
+    var PrenomE = document.getElementById("PrenomE").value
+    window.localStorage.setItem('UserPrenom', PrenomE);
+    window.location = window.location
 }
 function EmailEdit()
 {
-    document.getElementById("Email").innerHTML='<input type="text"/>&ensp; &ensp;<i class="fa-solid fa-check"></i>'
+    document.getElementById("Email").innerHTML='<input type="text" id="EmailE"/>&ensp; &ensp;<i onclick="EmailSave()" class="fa-solid fa-check"></i>'
+}
+function EmailSave()
+{
+    var EmailE = document.getElementById("EmailE").value
+    window.localStorage.setItem('UserEmail', EmailE);
+    window.location = window.location
 }
 function AgeEdit()
 {
-    document.getElementById("Age").innerHTML='<input type="text"/>&ensp; &ensp;<i class="fa-solid fa-check"></i>'
+    document.getElementById("Age").innerHTML='<input type="text" id="AgeE" />&ensp; &ensp;<i onclick="AgeSave()" class="fa-solid fa-check"></i>'
+}
+function AgeSave()
+{
+    var AgeE = document.getElementById("AgeE").value
+    window.localStorage.setItem('UserAge', AgeE);
+    window.location = window.location
 }
 function GradeEdit()
+{ 
+    document.getElementById("Grade").innerHTML='<input type="text" id="GradeE" />&ensp; &ensp;<i onclick="GradeSave()" class="fa-solid fa-check"></i>'
+}
+function GradeSave()
 {
-    document.getElementById("Grade").innerHTML='<input type="text"/>&ensp; &ensp;<i class="fa-solid fa-check"></i>'
+    var GradeE = document.getElementById("GradeE").value
+    window.localStorage.setItem('UserGrade', GradeE);
+    window.location = window.location
 }
 function AdresseEdit()
 {
-    document.getElementById("Adresse").innerHTML='<input type="text"/>&ensp; &ensp;<i class="fa-solid fa-check"></i>'
+    document.getElementById("Adresse").innerHTML='<input type="text" id="AdresseE" />&ensp; &ensp;<i onclick="AdresseSave()" class="fa-solid fa-check"></i>'
+}
+function AdresseSave()
+{
+    var AdresseE = document.getElementById("AdresseE").value
+    window.localStorage.setItem('UserAdresse', AdresseE);
+    window.location = window.location
 }
 function TelephoneEdit()
 {
-    document.getElementById("Telepehone").innerHTML='<input type="text"/>&ensp; &ensp;<i class="fa-solid fa-check"></i>'
+    document.getElementById("Telephone").innerHTML='<input type="text" id="TelephoneE" />&ensp; &ensp;<i onclick="TelephoneSave()" class="fa-solid fa-check"></i>'
+}
+function TelephoneSave()
+{
+    var TelephoneE = document.getElementById("TelephoneE").value
+    window.localStorage.setItem('UserTelephone', TelephoneE);
+    window.location = window.location
 }
 
 
